a21c517eefef2e95b034846b94d62e396715e84d
Oussama <oussama.aitbaziz@gmail.com>
2019-10-12 09:33:12 +0100

    added CORS for all requests

--- api/app.js
+++ api/app.js
@@ -5,12 +5,16 @@ const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
 
 // create the Express app
 const app = express();
+
+// enable all cors requests
+app.use(cors());
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
